9791faacbc0fb3b501c787e0e35e03c086e8975a
Otuekong idongesit <otuekongarthur@gmail.com>
2022-06-01 18:53:56 +0100

    First commit -includes production ready file

---
+++ index.js
@@ -0,0 +1,30 @@
+// Variables
+const button1 = document.getElementById("btn1");
+const button2 = document.getElementById("btn2");
+const button3 = document.getElementById("btn3");
+const button4 = document.getElementById("btn4");
+const button5 = document.getElementById("btn5");
+const display = document.getElementById("result");
+
+// Rating
+function selection(rating) {
+    display.innerHTML = rating;
+}
+button1.addEventListener("click", () => { selection(1) });
+button2.addEventListener("click", () => { selection(2) });
+button3.addEventListener("click", () => { selection(3) });
+button4.addEventListener("click", () => { selection(4) });
+button5.addEventListener("click", () => { selection(5) });
+// Card Flip
+function showThanks() {
+    let card = document.querySelector('#card');
+    let submit = document.querySelector('#submit-btn');
+    submit.addEventListener('click', function () {
+        card.classList.toggle('flipped');
+    });
+}
+button1.addEventListener("click", () => { showThanks() });
+button2.addEventListener("click", () => { showThanks() });
+button3.addEventListener("click", () => { showThanks() });
+button4.addEventListener("click", () => { showThanks() });
+button5.addEventListener("click", () => { showThanks() });
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["index.html", "index.js"],
+  theme: {
+    extend: {
+    },
+    colors: {
+      veryDarkBlue: 'hsl(216, 12%, 8%)',
+      darkBlue: 'hsl(213, 19%, 18%)',
+      mediumGrey: 'hsl(216, 12%, 54%)',
+      lightGrey: 'hsl(217, 12%, 63%)',
+      btnOrange: 'hsl(25, 97%, 53%)',
+      projectWhite: 'hsl(0, 0%, 100%)'
+    }
+  },
+  plugins: [],
+}
